4823696ba863e3702d6bc0727df7a9f92c9335a0
me@georgenishimura.com <thetmkay>
2015-12-09 17:11:26 +0000

    removed storyboard and added temp launch image

--- uovo/AppDelegate.m
+++ uovo/AppDelegate.m
@@ -19,6 +19,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     
     EventsTableViewController * rootController = [[EventsTableViewController alloc] init];
     UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:rootController];
